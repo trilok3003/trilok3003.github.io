1fac7c69a670b0889de4424b95f2df58a0b6df37
trilok <trilok@gaiasys.in>
2022-04-18 11:39:39 +0530

    my portfolio web

---
+++ docs/assets/data.ts
@@ -0,0 +1,91 @@
+export const educations = [
+  {name: 'MCA', year: '2011-2014', university: 'UPTU'},
+  {name: 'BSC', year: '2008-2011', university: 'Dr. Bhimrao Ambedkar University'},
+  {name: '12th', year: '2006-2008', university: 'Up Board'},
+  {name: '10th', year: '2006', university: 'Up Board'}
+]
+export const experience = [
+  {
+    designation: 'Senior Software  Engineer',
+    year: 'July, 2019 - Present',
+    company: 'Gaiasys Technology Pvt Ltd',
+    skills: ['Angular', 'Ionic', 'React']
+  },
+  {
+    designation: 'Software  Engineer',
+    year: 'Jan, 2019 - July 2019',
+    company: 'Biorev Studios Pvt Ltd',
+    skills: ['Angular']
+  },
+  {
+    designation: 'Software  Engineer',
+    year: 'Sep, 2014 - Jan 2019',
+    company: 'Object Freeze Pvt Ltd',
+    skills: ['Angular']
+  }
+]
+export const summary = ['I\'m a senior software engineer. I am having 7+ years of experience.  Currently, I am working at “Gaiasys Technology Pvt Ltd”  as  “Senior Software  Engineer”.',
+  'I am so passionate about Angular , Ionic , React and Front end Development - I enjoy learning new things. I love doing multitasking work.',
+  'and skilled, with the ability to think logically and creativity strong, as well as educational background in accordance with current expertise.',
+]
+
+export const knowledge = [
+  {name: 'Tools & Framework', items: ['Webstorm', 'Vs Code']},
+  {name: 'Interest', items: ['Music', 'Reading', 'Cricket', 'Movie']},
+  {name: 'Another Skills', items: ['Linux', 'Windows']}
+]
+export const skills = [
+  {
+    name: 'Skills',
+    items: [
+      {name: 'HTML', progress: 80},
+      {name: 'CSS', progress: 85},
+      {name: 'JAVASCRIPT', progress: 75},
+      {name: 'npm', progress: 50},
+      {name: 'Angular CLI', progress: 50},
+      {name: 'Angular', progress: 50},
+      {name: 'TypeScript', progress: 50},
+      {name: 'Git', progress: 50},
+      {name: 'Firebase', progress: 50},
+      {name: 'RxJS', progress: 50},
+      {name: 'Angular Material', progress: 50},
+      {name: 'Bootstrap 4', progress: 50},
+      {name: 'RESTful API integration', progress: 60}
+    ]
+  }
+]
+export const portfolio = [
+  {
+    company: 'Gaiasys', items: [
+      {name: 'Earther Web Product', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'Earther  Mobile App', description: '', techs: ['Angular', 'Ionic', 'Nodejs', 'TypeScript']},
+      {name: 'Daybook', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'NBFC', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'KRC INFRA PROJECTS', description: '', techs: ['Html', 'Css', 'Javascript', 'Wordpress']},
+    ]
+  },
+  {
+    company: 'Biorev', items: [
+      {name: 'XPLAT', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'XHOME', description: '', techs: ['Angular', 'Ionic', 'Nodejs', 'TypeScript']},
+      {name: 'XFLOOR', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'XDESIGN', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+    ]
+  },
+  {
+    company: 'Object Freeze', items: [
+      {name: 'JMD Hospital', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'Hotel Anjuman', description: '', techs: ['Angular', 'Ionic', 'Nodejs', 'TypeScript']},
+      {name: 'Hotel Kd', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'Handy craft', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'New Balaji', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'Crustncakes', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'Pandit Ji Nimantran', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'Vivah Creation', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'Deals Ka Baap', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'Object Freeze', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      {name: 'Cakes Out', description: '', techs: ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+      // {name: 'shoes/interior/school/srk college/inventory/training', description: '', techs : ['Angular', 'Angular Material', 'Nodejs', 'TypeScript']},
+    ]
+  }
+]
